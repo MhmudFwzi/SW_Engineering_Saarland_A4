fc2140d59a1fc3a6d7543fd2545420687f2891a3
fawzi <mahmoud.fawzi@dhc-vision.com>
2022-02-01 18:51:03 +0100

    sample configurations

---
+++ sample.py
@@ -0,0 +1,35 @@
+import random
+random.seed(1) #for replication
+
+with open("per_measures.csv") as f:
+    lines = f.readlines()
+
+distance_sets = [[], [], [], [], [], [], []]
+
+for l in lines[1:]:
+    id = l.split(",")[0]
+    feature_selections = l.split(",")[1:-1]
+    for i,f in enumerate(feature_selections):
+        feature_selections[i] = int(f)
+    distance = sum(feature_selections) - 3
+    distance_sets[distance].append(id)
+
+print(distance_sets)
+
+samples = []
+
+for i,d in enumerate(distance_sets):
+    if len(d) < 5:
+        samples.append(d)
+    else:
+        sample_ids = []
+        while len(sample_ids) != 4:
+            selection = random.randint(0, len(d) - 1)
+            if selection not in sample_ids:
+                sample_ids.append(selection)
+        sample = []
+        for id in sample_ids:
+            sample.append(d[id])
+        samples.append(sample)
+
+print(samples)
